915f35697a7e3b6723bb74fe8e43b4092b7ca71c
Karl Floersch <karl@karlfloersch.com>
2015-08-16 21:17:39 -0400

    Get the basic app structure setup

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-var express = require('express'),
-    routes = require('./routes/index');
+/* main module entry point */
+var cfg = require('_/config')
+var app = require('_/app')
 
-var app = express();
-
-var env = process.env.NODE_ENV || 'development';
-if ('development' == env) {
-}
-
-// Set up JSON and URL parsing in requests
-var bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-app.get('/get-posts', routes.getPosts);
-
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(cfg.port)
+log.info('app listening on port', cfg.port)
